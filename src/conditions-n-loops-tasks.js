b0778029f184aa24ace0a6d1a027ddb96f0783d0
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2024-01-12 06:00:19 +0200

    feat:add solution to convertToRomanNumerals task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -106,8 +106,29 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const romanNumerals = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    0: '',
+  };
+
+  const romanTenNumerals = {
+    1: 'X',
+    2: 'XX',
+    3: 'XXX',
+    0: '',
+  };
+  const qualityOfTens = Math.floor(num / 10);
+
+  return romanTenNumerals[qualityOfTens] + romanNumerals[num % 10];
 }
 
 /**
